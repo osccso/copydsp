3cf5eb4c4897a5aa5283dd839e0cc0b150674e91
SebasttianM <sebasttianvmarin@gmail.com>
2022-03-31 16:29:29 -0500

    globalContext created

--- src/Routes/AppRouters.js
+++ src/Routes/AppRouters.js
@@ -1,15 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../components/Home';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../components/Home";
+import { SesionContext } from "../useContext/SessionContext";
+import React, { useState } from "react";
 
 function AppRouters() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    const [sessions, setSessions] = useState({
+        description: "",
+        primary_topic: "",
+    });
+    return (
+        <SesionContext.Provider value={{ sessions, setSessions }}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </SesionContext.Provider>
+    );
 }
 
 export default AppRouters;
