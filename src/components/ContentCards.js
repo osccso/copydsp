87356f70b6bc31cbc62e1987033284076480fc33
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-31 15:13:07 -0500

    comenzando cards

--- src/components/ContentCards.js
+++ src/components/ContentCards.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const ContentCards = () => {
-  return <div>ContentCards</div>;
+  return (
+    <div className='contentCards'>
+      <div>
+        <Navbar />
+      </div>
+      <div className='ContCards'></div>
+    </div>
+  );
 };
 
 export default ContentCards;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
 import ContentCards from './ContentCards';
 import SideBar from './SideBar';
 
@@ -7,10 +6,7 @@ const Home = () => {
   return (
     <div className='home'>
       <SideBar />
-      <div>
-        <Navbar />
-        <ContentCards />
-      </div>
+      <ContentCards />
     </div>
   );
 };
