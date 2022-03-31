b479ac1ddc6fadfc4ffbc79e5019890b1d77be52
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-31 16:16:59 -0500

    sidebar nombrs

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -21,14 +21,14 @@ const SideBar = () => {
       <div className='Acordion'>
         <Accordion defaultActiveKey='0'>
           <Accordion.Item eventKey='0'>
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Header>Industry Segment</Accordion.Header>
             <Accordion.Body>
               <input type='checkbox' />
               <label>industry</label>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey='1'>
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Header>Primary Topic</Accordion.Header>
             <Accordion.Body>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -40,28 +40,28 @@ const SideBar = () => {
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey='2'>
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Header>Other Collections</Accordion.Header>
             <Accordion.Body>
               <input type='checkbox' />
               <label>industry</label>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey='3'>
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Header>Session Type</Accordion.Header>
             <Accordion.Body>
               <input type='checkbox' />
               <label>industry</label>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey='4'>
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Header>Audience Type</Accordion.Header>
             <Accordion.Body>
               <input type='checkbox' />
               <label>industry</label>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey='5'>
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Header>Audience Level</Accordion.Header>
             <Accordion.Body>
               <input type='checkbox' />
               <label>industry</label>
