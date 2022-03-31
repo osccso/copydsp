4c2bde683cbd1ee1b7bfcfbbce93619ad83ac528
SebasttianM <sebasttianvmarin@gmail.com>
2022-03-31 17:05:01 -0500

    Global State now in use

--- src/Routes/AppRouters.js
+++ src/Routes/AppRouters.js
@@ -1,22 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
-import { SesionContext } from "../useContext/SessionContext";
+import { SessionContext } from "../useContext/SessionContext";
 import React, { useState } from "react";
+import RelatedTopics from "../components/RelatedTopics";
 
 function AppRouters() {
     const [sessions, setSessions] = useState({
-        description: "",
-        primary_topic: "",
+        description: "Global changes in eastern economy",
+        primary_topic: "Economy"
     });
     return (
-        <SesionContext.Provider value={{ sessions, setSessions }}>
+        <SessionContext.Provider value={{ sessions, setSessions }}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/topics" element={<RelatedTopics />}/>
                 </Routes>
             </BrowserRouter>
-        </SesionContext.Provider>
+        </SessionContext.Provider>
     );
 }
 
